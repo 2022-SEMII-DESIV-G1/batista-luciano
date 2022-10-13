369f51f60c27edc681c9f0fcf0452ca05c57b755
luciano Batista <luciano@gmail.com>
2022-10-12 19:15:49 -0500

    ingresando el html y el css

---
+++ lab8/codigo.js
@@ -0,0 +1,9 @@
+function calcular(){
+const Cadena = document.getElementById("texto").value
+document.getElementById("respuesta").style.visibility="visible";
+document.getElementById("respuesta").innerHTML="letras"+Cadena;
+
+}
+function limpiar(){
+
+}
