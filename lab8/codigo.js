0d23013c7ca564bdff107e297876e383d28e6c36
luciano Batista <luciano@gmail.com>
2022-10-19 17:53:53 -0500

    modificando diseno

--- lab8/codigo.js
+++ lab8/codigo.js
@@ -1,7 +1,4 @@
 function calcular(){
-const Cadena = document.getElementById("texto").value
-document.getElementById("respuesta").style.visibility="visible";
-document.getElementById("respuesta").innerHTML="letras"+Cadena;
 
 }
 function limpiar(){
